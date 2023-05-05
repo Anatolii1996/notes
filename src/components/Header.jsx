94cc1e79a68884e756b3676d1a7c55f0965899e5
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-05-05 18:38:36 +0300

    add antd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,16 @@ import { HiPlus } from "react-icons/hi";
 import { BsTrash3 } from "react-icons/bs";
 import { SlNote } from "react-icons/sl";
 
-
-
 const Header = () => {
   return (
     <div className="header">
       <div className="btn_group">
-        <HiPlus/>
-        <BsTrash3/>
-        <SlNote/>
+        <HiPlus />
+        <BsTrash3 />
+        <SlNote />
       </div>
       <div className="search">
-        <input type="text"  placeholder="Search"/>
+       
       </div>
     </div>
   );
