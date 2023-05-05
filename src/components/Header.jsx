72200e4c571045baa635222f801d1b718c2afe12
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-05-05 17:50:21 +0300

    add buttons to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+import { HiPlus } from "react-icons/hi";
+import { BsTrash3 } from "react-icons/bs";
+import { SlNote } from "react-icons/sl";
+
+
+
 const Header = () => {
   return (
-    <div className="header_wrap">
-      <div className="btn_group"></div>
-      <div className="search"></div>
+    <div className="header">
+      <div className="btn_group">
+        <HiPlus/>
+        <BsTrash3/>
+        <SlNote/>
+      </div>
+      <div className="search">
+        <input type="text"  placeholder="Search"/>
+      </div>
     </div>
   );
 };
