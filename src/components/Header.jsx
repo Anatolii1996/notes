1ec513908954755d9519bf6286de100270e8e111
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-05-05 17:25:42 +0300

    add sass and react-icon

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header_wrap">
+      <div className="btn_group"></div>
+      <div className="search"></div>
+    </div>
+  );
+};
+export default Header;
