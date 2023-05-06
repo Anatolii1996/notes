5f2f95c13a6975921e703929c8396c8696d9ec16
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-05-06 17:34:25 +0300

    add to indexedDB

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
+import { useEffect } from "react";
 import "../App.scss";
 import Header from "./Header";
 
+// const=[{
+  
+// }]
+
+const idb = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB;
+
+const createCollection = () => {
+  if (!idb) {
+    console.log("Error browser");
+    return
+  }
+  console.log(idb);
+  const request = idb.open("MessagesDB", 2);
+
+  request.onerror = (event) => {
+    console.log("Error", event);
+  };
+
+  request.onupgradeneeded = (event) => {
+    const db = request.result;
+
+    if(!db.objectStoreNames.contains("MessageStore")){
+      db.createObjectStore("MessageStore")
+    }
+  };
+
+  request.onsuccess = () => {
+
+  }
+}
+
 function App() {
+  useEffect(() => {
+    createCollection()
+  }, [])
   return (
     <div className="App">
-      <Header/>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Message.js
@@ -0,0 +1,17 @@
+import { uuid } from 'uuidv4';
+
+export const message = [
+    {
+        id: uuid(),
+        title: "Wow what a cool note. Wow",
+        text: "This is an amazing note. Can you believe how great this note is? It`s the best note."
+    },
+    { id: uuid(),
+        title:"Can you check the flight schedule?",
+        text: "how:"
+    },
+    { id: uuid(),
+        title:"OSXDaily.com example",
+        text: "Locked"
+    },
+]
