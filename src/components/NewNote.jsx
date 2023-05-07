a5b281f15779912d42721df10ef222b0f16e4330
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-05-07 14:06:02 +0300

    add currennt time

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import moment from 'moment';
 
 const NewNote = () => {
   const [newNote, setNewNote] = useState("");
+  const [currentTime, setCurrentTime] = useState(moment().format("LL HH:mm"));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(moment().format("LL HH:mm"));
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="new_note">
+      <p className="time">{currentTime}</p>
       <textarea name="" id="" cols="100" rows="10"></textarea>
     </div>
   );
