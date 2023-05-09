330056f38fc31059e9ed7b21bb5f4d74971d5230
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-05-09 15:23:46 +0300

    add modal to delete note

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,53 @@
+import { useState } from "react";
 import { HiPlus } from "react-icons/hi";
 import { BsTrash3 } from "react-icons/bs";
 import { SlNote } from "react-icons/sl";
-import { AutoComplete } from "antd";
-import { SearchOutlined } from "@ant-design/icons";
+import { AutoComplete, Modal } from "antd";
+import { SearchOutlined, ExclamationCircleFilled } from "@ant-design/icons";
 import { Link, Outlet } from "react-router-dom";
 
-const Header = ({removeRecord, idClicked, notes}) => {
+
+
+
+const Header = ({ removeRecord, idClicked, notes }) => {
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+
+  const { confirm } = Modal;
+const showDeleteConfirm = () => {
+    confirm({
+      title: 'Are you sure you want to delete this note?',
+      icon: <ExclamationCircleFilled />,
+      content: '',
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      onOk() {
+        removeRecord(notes.find((el) => el.id == idClicked).initialText);
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
+  };
+
   return (
     <>
       <div className="header">
         <div className="btn_group">
-          <Link to="/new" >
+          <Link to="/new">
             <HiPlus />
           </Link>
 
-          <BsTrash3 className={!idClicked?"inactive":""}
-           onClick={()=>{removeRecord(notes.find((el)=>el.id==idClicked).initialText)} }
+          <BsTrash3
+            className={!idClicked ? "inactive" : ""}
+            onClick={showDeleteConfirm} type="dashed"
+            // onClick={() => {
+            //   removeRecord(notes.find((el) => el.id == idClicked).initialText);
+            // }}
           />
-          <Link to="/change" className={!idClicked?"inactive":""}>
-          <SlNote />
+          <Link to="/change" className={!idClicked ? "inactive" : ""}>
+            <SlNote />
           </Link>
-          
         </div>
         <div className="search">
           <SearchOutlined
